a11f66dbceb9b349aff3d7f02fa9794ecdd0c30a
Monica Murray <Monica.rickerby@gmail.com>
2023-01-25 22:37:17 +0000

    Add functions for lightsOn and showTurns

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -3,19 +3,20 @@ let game = {
     score: 0,
     playerMoves: [],
     choices: ["button1", "button2", "button3", "button4"],
+    turnNumber: 0;
 };
 
 function newGame() {
     game.score = 0;
     game.playerMoves = [];
     game.currentGame = [];
-    showScore();
+    // showScore();
     addTurn();
 }
 
-function showScore() {
-    document.getElementById("score").innerText = game.score;
-}
+// function showScore() {
+//     document.getElementById("score").innerText = game.score;
+// }
 
 // needs to do 3 things - clear the playerMoves, add at random one of the buttons from the choices array, and showTurns()
 function addTurn() {
@@ -24,4 +25,25 @@ function addTurn() {
     // showTurns();
 }
 
-module.exports = { game, newGame, showScore, addTurn };
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove(circ + "light");
+    }, 400);
+}
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber ++;
+        if (game.turnNumber>= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+
+
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns};
